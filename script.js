86c0597caf0c0e513dff10e85791bd20ac460bb6
Sanhita <sacred.profiles.02@gmail.com>
2024-03-17 14:15:37 +0530

    first

---
+++ script.js
@@ -0,0 +1,19 @@
+function openLink(url) {
+    window.location.href = url;
+}
+
+window.onload = function(){
+    document.querySelector('.cont_modal').className = "cont_modal";    
+    }
+    var c = 0;
+    function open_close(){
+      if(c % 2 == 0){    
+    document.querySelector('.cont_modal').className = "cont_modal cont_modal_active";  
+    c++;
+      }else {
+    document.querySelector('.cont_modal').className = "cont_modal";  
+    c++;    
+      }  
+    } 
+ 
+    
