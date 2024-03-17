ca41c75fd0c36887efd6bef94ce2f9f821713769
Sanhita <sacred.profiles.02@gmail.com>
2024-03-17 15:59:38 +0530

    first

--- script.js
+++ script.js
@@ -16,4 +16,23 @@ window.onload = function(){
       }  
     } 
  
-    
+
+
+    $(document).ready(function() {
+      $(".button").on('click', function() {
+          $(this).addClass("clicked");
+  
+          setTimeout(function() {
+              $(".button").addClass("complete");
+          }, 5000);
+          
+          setTimeout(function() {
+              $(".button").removeClass("clicked");
+          }, 8000);
+          
+          setTimeout(function() {
+              $(".button").removeClass("complete");
+          }, 8000);
+      });
+  });
+  
